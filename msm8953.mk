89c5ab724742fea602dcfe82d6ae205f06d50bc3
Rajshekar Eashwarappa <reashw@codeaurora.org>
2018-03-14 15:38:59 +0530

    msm8953-common: Build 2.0 healthd hal

--- msm8953.mk
+++ msm8953.mk
@@ -182,6 +182,8 @@ PRODUCT_COPY_FILES += \
 # Healthd
 PRODUCT_PACKAGES += \
     chargeonlymode
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-service
 
 # HIDL
 PRODUCT_PACKAGES += \
