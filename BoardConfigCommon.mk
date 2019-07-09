ff13bb091c0765169ab2c347d4f99021754f866e
PrateekPunetha <prateekpunetha@gmail.com>
2019-07-09 15:17:36 +0000

    msm8953-common: Declare power and thermal hals

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -170,6 +170,7 @@ TARGET_PER_MGR_ENABLED := true
 # Power
 TARGET_HAS_NO_WLAN_STATS := true
 TARGET_USES_INTERACTION_BOOST := true
+TARGET_POWER_HAL := true
 
 # Qualcomm
 BOARD_USES_QCOM_HARDWARE := true
@@ -184,6 +185,9 @@ TARGET_RECOVERY_FSTAB := $(VENDOR_PATH)/rootdir/fstab_AB.qcom
 else
 TARGET_RECOVERY_FSTAB := $(VENDOR_PATH)/rootdir/fstab.qcom
 endif
+
+# Thermal
+TARGET_THERMAL_HAL := true
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
