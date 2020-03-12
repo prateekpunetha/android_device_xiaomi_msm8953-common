4bfb78937f2ce7c073f2c148bd28b74c2e026ad6
PrateekPunetha <prateekpunetha@gmail.com>
2020-03-12 16:57:08 +0000

    msm8953-common: dont include device/qcom/sepolicy/sepolicy.mk PA already includes legacy-um.mk
    Signed-off-by: PrateekPunetha <prateekpunetha@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -198,7 +198,7 @@ endif
 TARGET_THERMAL_HAL := true
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+#include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # Wi-Fi
