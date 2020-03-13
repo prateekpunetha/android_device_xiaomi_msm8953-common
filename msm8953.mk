90565de518982eb0f720293aeae5a83f52415f9b
PrateekPunetha <prateekpunetha@gmail.com>
2020-03-13 13:00:45 +0000

    [DNM]: msm8953-common: rip doze for now
    Signed-off-by: PrateekPunetha <prateekpunetha@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -141,10 +141,6 @@ PRODUCT_PACKAGES += \
     libgenlock \
     libtinyxml
 
-# Doze mode
-PRODUCT_PACKAGES += \
-    XiaomiDoze
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
