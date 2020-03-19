9dfab8c67bc080998476c95e159708ec49cf84ca
PrateekPunetha <prateekpunetha@gmail.com>
2020-03-19 07:41:34 +0000

    msm8953-common: Disable camera2api
    This reverts commit 20da1fb4500f8de06c64fa3cc081902b60e10074.

--- msm8953.mk
+++ msm8953.mk
@@ -117,6 +117,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.msm8953 \
     libmm-qcamera \
+    Snap
 
 # Configstore
 PRODUCT_PACKAGES += \
--- prop.mk
+++ prop.mk
@@ -51,8 +51,7 @@ persist.camera.gyro.disable=0 \
 persist.camera.isp.clock.optmz=0 \
 persist.camera.stats.test=5 \
 persist.vendor.qti.telephony.vt_cam_interface=1 \
-vidc.enc.dcvs.extra-buff-count=2 \
-persist.camera.HAL3.enabled=1
+vidc.enc.dcvs.extra-buff-count=2
 
 # Cne/Dpm
 PRODUCT_PROPERTY_OVERRIDES += \
